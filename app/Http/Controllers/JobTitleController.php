26a06a83d9f9f39a9827a03d7f622bc97069e8d5
André Luiz Girol <andregirol@gmail.com>
2018-04-09 17:39:56 -0300

    Criando recursos para Cargo (Job Title)

---
+++ app/Http/Controllers/JobTitleController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JobTitle;
+use Illuminate\Http\Request;
+
+class JobTitleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\JobTitle  $jobTitle
+     * @return \Illuminate\Http\Response
+     */
+    public function show(JobTitle $jobTitle)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\JobTitle  $jobTitle
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(JobTitle $jobTitle)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\JobTitle  $jobTitle
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, JobTitle $jobTitle)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\JobTitle  $jobTitle
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(JobTitle $jobTitle)
+    {
+        //
+    }
+}
