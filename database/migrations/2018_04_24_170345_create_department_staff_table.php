311494fdaad2b8cb37af66ba8326aadcbd364f13
André Luiz Girol <andregirol@gmail.com>
2018-04-24 17:06:54 -0300

    Migration pivot department_staff

---
+++ database/migrations/2018_04_24_170345_create_department_staff_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDepartmentStaffTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('department_staff', function (Blueprint $table) {
+            $table->integer('users_id')->unsigned()->index();
+            $table->foreign('users_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->integer('departments_id')->unsigned()->index();
+            $table->foreign('departments_id')
+                ->references('id')
+                ->on('departments')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('department_staff');
+    }
+}
