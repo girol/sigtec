36895a8c0af6109593605282aebcf39e07007980
André Luiz Girol <andregirol@gmail.com>
2018-04-24 18:24:03 -0300

    Add Recurso Ticket Status

---
+++ database/migrations/2018_04_24_172227_create_ticket_statuses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTicketStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ticket_statuses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('alias');
+            $table->string('bs_label')->comment("Short name for bootstrap labels");
+            $table->string('icon')->comment("Short name for fontfyed icons as Font Awesome");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ticket_statuses');
+    }
+}
