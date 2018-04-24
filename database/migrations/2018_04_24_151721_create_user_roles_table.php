5f9ec6a6e9fe2b6b61f75de6c2a8d8d316d09b11
André Luiz Girol <andregirol@gmail.com>
2018-04-24 15:22:21 -0300

    Migration pivot user_roles

---
+++ database/migrations/2018_04_24_151721_create_user_roles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_roles', function (Blueprint $table) {
+            $table->integer('users_id')->unsigned()->index();
+            $table->foreign('users_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->integer('roles_id')->unsigned()->index();
+            $table->foreign('roles_id')
+                ->references('id')
+                ->on('roles')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_roles');
+    }
+}
