bfbaafb574c186f094b49f27f2f5895a3d5b7f17
André Luiz Girol <andregirol@gmail.com>
2018-05-16 12:46:51 -0300

    Criada migration de anotações

---
+++ database/migrations/2018_05_16_124445_create_notes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('text');
+            $table->timestamps();
+
+            # FK
+            $table->integer('ticket_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            
+            # Constraints
+            $table->foreign('ticket_id')->references('id')->on('tickets');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+}
