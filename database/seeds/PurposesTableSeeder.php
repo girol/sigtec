b9f30402dd62a1aa5fa4de18351d78ab59ac88a3
André Luiz Girol <andregirol@gmail.com>
2018-10-10 17:38:01 -0300

    Fixando IDs em Purposes Seeder

--- database/seeds/PurposesTableSeeder.php
+++ database/seeds/PurposesTableSeeder.php
@@ -14,26 +14,31 @@ class PurposesTableSeeder extends Seeder
         $now = date("Y-m-d H:i:s");
         DB::table('purposes')->insert([
             [
+                'id' => 1,
                 'description' => 'Projetos Didáticos',
                 "created_at" => $now,
                 "updated_at" => $now,
             ],
             [
+                'id' => 2,
                 'description' => 'Projetos de Pesquisa',
                 "created_at" => $now,
                 "updated_at" => $now,
             ],
             [
+                'id' => 3,
                 'description' => 'Projetos de Laboratório',
                 "created_at" => $now,
                 "updated_at" => $now,
             ],
             [
+                'id' => 4,
                 'description' => 'Departamento',
                 "created_at" => $now,
                 "updated_at" => $now,
             ],
             [
+                'id' => 5,
                 'description' => 'Atendimento Externo',
                 "created_at" => $now,
                 "updated_at" => $now,
