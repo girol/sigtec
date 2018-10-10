0ff7b1151640dfca8efd20c12a51d84e0a24e27f
André Luiz Girol <andregirol@gmail.com>
2018-10-10 17:46:54 -0300

    Definindo um base Seeder para Dev

---
+++ database/seeds/dev/DevSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DevSeeder extends Seeder
+{
+    /**
+     * Seeder wrapper para dados de desenvolvimento utilizando database como autenticação
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(DevUsersTableSeeder::class);
+        $this->call(DevUserRolesTableSeeder::class);
+    }
+}
