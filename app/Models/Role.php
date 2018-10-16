e855dd2a7a05e0940ac2668d4fcc4d46da1b1995
André Luiz Girol <andregirol@gmail.com>
2018-10-16 15:51:14 -0300

    Role retorna usuários pertencentes a ela

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    # Relationships
+    public function users()
+    {
+        return $this->belongsToMany('App\User');
+    }
 }
