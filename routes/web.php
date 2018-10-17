2f3c77b640e0dd40b326e0b62beeec1b48879aa3
André Luiz Girol <andregirol@gmail.com>
2018-10-17 18:22:34 -0300

    Rotas - Customizando Auth::routes()
    Documentado no arquivo routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,28 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
+/* ====================================================
+ *  Custom Auth Routes
+ * ====================================================
+ * Taken from: /vendor/laravel/framework/src/Illuminate/Routing/Router.php
+ *
+ * Auth Routes without the 'register' method 
+*/
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
-Auth::routes();
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
 
 Route::get('/home', 'HomeController@index')->name('home');
